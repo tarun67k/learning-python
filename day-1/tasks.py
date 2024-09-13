3e3ffd81ba9a548ef972b8cd0da523ba987376fe
tarun67k <tarunkolli553@gmail.com>
2024-09-13 16:57:58 +0530

    execute

---
+++ day-1/tasks.py
@@ -0,0 +1,17 @@
+print("Hello, World")
+
+name = input("Enter Name - ")
+print("Hii", name)
+
+print(4+6)
+
+age = int(input("Enter Age - "))
+next_year = 1 
+print(age+next_year)
+
+n = 5
+print("*" * n)
+print("*" * n)
+print("*" * n)
+print("*" * n)
+print("*" * n)
