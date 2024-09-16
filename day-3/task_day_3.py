db42b8d22deb202298c0db460f476c163f457861
tarun67k <tarunkolli553@gmail.com>
2024-09-16 11:17:02 +0530

    development

---
+++ day-3/task_day_3.py
@@ -0,0 +1,21 @@
+a = 10
+b = 6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+
+user = int(input())
+print(user>10)
+
+print(a%b)
+
+print(user > 10 < 20)
+
+s1 = "Tarun"
+s2 = "Kolli"
+l1 = len(s1)
+l2 = len(s2)
+print(l1>l2)
+
