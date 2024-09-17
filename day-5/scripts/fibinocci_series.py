af1e3dd7198a27f68fa793de051a5d3bed4c20d6
tarun67k <tarunkolli553@gmail.com>
2024-09-17 13:31:24 +0530

    Update fibinocci_series.py

---
+++ day-5/scripts/fibinocci_series.py
@@ -0,0 +1,15 @@
+#Use a loop to print the Fibonacci sequence up to a certain number
+
+limit = int(input("Enter a number - ")) 
+a = 0
+b = 1
+f = [a,b]
+for i in range(limit):
+    c = a+b 
+    f.append(c)
+    a = b 
+    b = c 
+print(f)
+print(f[limit])
+print(sum(f))
+
