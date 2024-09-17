2745ce23efa304b5299f955773f3bcd5fdcc67e8
tarun67k <tarunkolli553@gmail.com>
2024-09-17 13:52:25 +0530

    resolved conflict

--- day-5/scripts/fibinocci_series.py
+++ day-5/scripts/fibinocci_series.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #Use a loop to print the Fibonacci sequence up to a certain number
 
 limit = int(input("Enter a number - "))  # You can change this value
@@ -15,38 +13,3 @@ for i in range(limit):
 print(f)
 print(f[limit])
 print(sum(f))
-
-=======
-#Use a loop to print the Fibonacci sequence up to a certain number
-
-limit = int(input("Enter a number - ")) 
-a = 0
-b = 1
-f = [a,b]
-for i in range(limit):
-    c = a+b 
-    f.append(c)
-    a = b 
-    b = c 
-print(f)
-print(f[limit])
-print(sum(f))
-
->>>>>>> af1e3dd7198a27f68fa793de051a5d3bed4c20d6
-=======
-#Use a loop to print the Fibonacci sequence up to a certain number
-
-limit = int(input("Enter a number - ")) 
-a = 0
-b = 1
-f = [a,b]
-for i in range(limit):
-    c = a+b 
-    f.append(c)
-    a = b 
-    b = c 
-print(f)
-print(f[limit])
-print(sum(f))
-
->>>>>>> 8f00978c848cba2650c0c1451c991718bdc0e88b
