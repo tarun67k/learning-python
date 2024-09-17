04e2c57ef0ec002078ef4556a194d52c61bffc1a
tarun67k <tarunkolli553@gmail.com>
2024-09-17 16:48:00 +0530

    Adding scripts_day-2

---
+++ day-3/scripts_day-3/greater.py
@@ -0,0 +1,3 @@
+#Take user input and check if it is greater tha 10.
+user = int(input("Eneter a number - "))
+print(user>10)
---
+++ day-3/scripts_day-3/modulus.py
@@ -0,0 +1,4 @@
+#Calculate the remainder of two number (modular)
+a = int(input("Enter a value - "))
+b = int(input("Enter b value - "))
+print(a%b)
